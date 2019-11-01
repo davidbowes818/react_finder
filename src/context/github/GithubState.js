778c4f435a4c1dfbb3daa760ace26f07884c832b
iloveket <davidrb2017@outlook.com>
2019-11-01 11:16:45 +0000

    Added reducer and app level state

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -22,6 +22,18 @@ const GithubState = props => {
 
   // SEARCH USERS
 
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
+
   // GET USER
 
   // GET REPOS
@@ -30,13 +42,16 @@ const GithubState = props => {
 
   // SET LOADING
 
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
